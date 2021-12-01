79df0f2cf831f83f1122fedd473e2bd8a5761677
dellahi <el.waghf.mohamed.abdellahi@gmail.com>
2021-12-01 21:44:45 +0100

    88%

--- Algorithms/FishingWithStick.js
+++ Algorithms/FishingWithStick.js
@@ -10,75 +10,102 @@
  var lines = []
  var noGarbage= true;
  var closestGarbage;
+ var stop = false
  for (let i = 0; i < H; i++) {
      const row = readline().split('');
      lines.push(row)
  }
- 
- var firstRow = lines[0]
- let rodIndex = firstRow.indexOf('|')
+ let firstRow = lines[0]
+ var rodIndex = firstRow.indexOf('|')
  if(rodIndex == -1) rodIndex = firstRow.indexOf('C')
  
- currentD =='RIGHT' ? closestGarbage = 0 : closestGarbage=W-rodIndex
+ function findTheHook(indice) {
+     let found = false
+     lines.forEach(e => {
+     if(e[indice] == 'C') found = true;
+     })
+     return found
+ }
  
  findClosestGarbage = function() {    
+     currentD =='RIGHT' ? closestGarbage = -1 : closestGarbage = W
      for (let i = 0; i < H; i++) {
          let element = lines[i]
+         if(element[rodIndex] == 'C' ) break;
          let leftSide = element.slice(0, rodIndex)
          let rightSide = element.slice(rodIndex+1,W)
          switch(currentD){
                  case 'RIGHT' :
                              leftSide.forEach((e2,i) => {
-                                 if(!'.><'.includes(e2) && i >= closestGarbage ) {closestGarbage=i; noGarbage= false} 
+                                 if(!'.><'.includes(e2) && i >= closestGarbage ) {
+                                     let stopped = false;
+                                     for (let k= i; k < rodIndex ; k ++){
+                                     if (leftSide[k]=='<') { stopped = true ; break }
+                                     }
+                                     if(!stopped) { closestGarbage = i ; noGarbage= false}
+                                     } 
                                  })
                              break;
                  case 'LEFT' : rightSide.forEach((e2,i) => {
-                                 if(!'.><'.includes(e2) && i <= closestGarbage && i!=rodIndex) {closestGarbage=i; noGarbage= false} 
+                                 if(!'.><'.includes(e2) && i <= closestGarbage) {
+                                     let stopped = false;
+                                     for (let j= 0 ; j <= i ; j ++){
+                                     if (rightSide[j]=='>') { stopped = true ; break }
+                                     }
+                                     if(!stopped) { closestGarbage = i ; noGarbage= false}
+                                     } 
                                  })
                              
                              break;
              }
-         if(element[rodIndex] == 'C' ) break;
      }
  }
- findClosestGarbage();
  
- lines.forEach( (element ) => {
-     let leftSide = element.slice(0, rodIndex)
-     let rightSide = element.slice(rodIndex+1,W)
-     leftSide.forEach( (e, i) => {
-         if ( e=='>') {
-                     for (let k= i; k < rodIndex ; k ++){
-                     if (!'>.'.includes(leftSide[k])) break ;
-                     if (k == rodIndex-1) { 
-                         if(!noGarbage) {
-                             if(currentD=='LEFT' && (rodIndex - i) < closestGarbage) answer++ ;
-                             if (currentD=='RIGHT' && i > closestGarbage) answer++ ;
+ fish = function() {
+     lines.forEach( (element ) => {
+         let leftSide = element.slice(0, rodIndex)
+         let rightSide = element.slice(rodIndex+1,W)
+         if(!stop){
+             leftSide.forEach( (e, i) => {
+             if ( e=='>') {
+                         for (let k= i; k < rodIndex ; k ++){
+                         if (!'>.'.includes(leftSide[k])) break ;
+                         if (k == rodIndex-1) { 
+                             if(!noGarbage) {
+                                 if(currentD=='LEFT' && (rodIndex - i) <= closestGarbage+1) answer++ ;
+                                 if (currentD=='RIGHT' && i >= closestGarbage) answer++
+                             }
+                             else answer++;
                          }
-                         else answer++;
-                     }
-                 }       
-         }
-     })
-      rightSide.forEach( (e,index) => {
-         if ( e =='<') {
-             for (let j= 0 ; j <= index ; j ++){
-                 if (!'<.'.includes(rightSide[j])) { break} ;
-                 if ( j == index) {
-                     if(!noGarbage) {
-                         if(currentD=='RIGHT' && (rodIndex - closestGarbage) >= index ) answer++
-                         if (currentD=='LEFT' && index <= closestGarbage) answer++
-                     }
-                     else answer ++;
+                     }       
                  }
-             }           
+             })
+             rightSide.forEach( (e,index) => {
+                 if ( e =='<') {
+                     for (let j= 0 ; j <= index ; j ++){
+                         if (!'<.'.includes(rightSide[j])) { break} ;
+                         if ( j == index) {
+                             if(!noGarbage) {
+                                 if(currentD=='RIGHT' && (rodIndex - closestGarbage) >= index+1 ) answer++ 
+                                 if (currentD=='LEFT' && index <= closestGarbage) answer++
+                             }
+                             else answer ++;
+                         }
+                     }           
+                 }
+             })
          }
-     })
+         if(element[rodIndex] == 'C') stop=true
      });
+ }
+ 
+ if( 0<H<256 && 0<W<256 && ['LEFT','RIGHT'].includes(currentD) && findTheHook){
+     findClosestGarbage();
+     fish();
+ }
      
  
  // Write an answer using console.log()
- 
- // To debug: console.error('Debug messages...');
  console.log(answer);
+ // To debug: console.error('Debug messages...');
  
